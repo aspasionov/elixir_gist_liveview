5b9862b77055ff2593e1d845ec0b1133bfa3df5f
Artem Spasionov <spasia3run@gmail.com>
2024-10-25 11:14:37 +0300

    update all gists page

--- lib/elixir_gist_web/live/all_gists_live.ex
+++ lib/elixir_gist_web/live/all_gists_live.ex
@@ -3,6 +3,8 @@ defmodule ElixirGistWeb.AllGistsLive do
 
   alias ElixirGist.Gists
 
+  alias ElixirGistWeb.Utilities.DateFormat
+
   def mount(_params, _session, socket) do
     {:ok, socket}
   end
@@ -14,31 +16,67 @@ defmodule ElixirGistWeb.AllGistsLive do
   end
 
   def gist(assigns) do
-    {:ok, relative_time} = Timex.format(assigns.gist.updated_at, "{relative}", :relative)
-
     assigns =
       assigns
-      |> assign(relative_time: relative_time)
+      |> assign(relative_time: DateFormat.get_relative_time(assigns.gist.updated_at))
 
     ~H"""
-    <div class="border borde-white rounded-md p-2 mb-6 shadow-sm shadow-white">
+    <div class="border borde-white rounded-md pt-2 mb-6 shadow-sm shadow-white">
       <div>
-        <div class="flex justify-between items-center">
-          <div>
+        <div class="flex justify-between items-center px-2">
+          <div class="flex items-center">
+          <div class=" w-8 h-8 mr-1 flex items-center justify-center rounded-full border-[1px] border-emLavander">
+            <img src="/images/user-image.svg" alt="" class=" w-4 h-4" />
+          </div>
             <span class="font-bold">
           <%= @current_user.email %>
         </span>/
             <span class="text-emLavander"><%= @gist.name %></span>
           </div>
-          <div class="font-bold">
-            <%= @relative_time %>
+          <div class="flex items-center">
+
+            <img src="/images/comment.svg" alt="" class="w-6 h-6 mr-1">
+            <span class=" mr-2">0</span>
+            <img src="/images/BookmarkOutline.svg" alt="" class="w-6 h-6 mr-1" >
+            <span class="">0</span>
           </div>
         </div>
       </div>
-      <div class="text-sm mt-4 text-emLavander-light">
+      <div class="flex justify-between items-center">
+      <div class="text-sm mt-4 text-emLavander-light px-2">
         <%= @gist.description %>
+      </div>
+      <div class="text-sm mt-4 text-white px-2"><%= @relative_time %></div>
+      </div>
+
+      <div class="mt-4 js-line-parent flex w-full" phx-update="ignore" id="gist-wrap" >
+      <textarea readonly id={"syntax-numbers=#{@gist.id}"} class="syntax-numbers border-none"></textarea>
+        <div
+          id={"highlight-wrap=#{@gist.id}"}
+          class="syntax-area w-full border-none"
+          phx-hook="Highlight"
+          data-name={@gist.name}
+        >
+            <pre>
+            <code class="language-elixir">
+              <%= get_preview_text(@gist) %>
+            </code>
+          </pre>
+        </div>
       </div>
     </div>
     """
   end
+
+  defp get_preview_text(gist) when not is_nil(gist.markup_text) do
+    lines = String.split(gist.markup_text, "\n")
+    if length(lines) > 10 do
+      Enum.take(lines, 9) ++ ["..."] |> Enum.join("\n")
+    else
+      gist.markup_text
+    end
+
+  end
+
+  defp get_preview_text(_gist), do: ""
 end
